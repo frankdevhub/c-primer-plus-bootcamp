3140aa090d4a66de701428d97c89ccd614babea8
frankdevhub <frankdevhub@gmail.com>
2019-08-18 04:10:58 +0800

    字符串指针的使用

---
+++ Dairy/2019/201908/20190818-20190824/字符串指针使用/字符串指针使用.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char * foo = "this is a foo";
+	char * f = foo;
+	cout << *f; //输出元素
+	cout << f; //字符串变量的指针
+
+
+
+
+	system("pause");
+}
