96b83d762e5fa38809b5f9418b680b89ee7a6828
frankdevhub <frankdevhub@gmail.com>
2019-08-18 05:38:52 +0800

    函数与指针

--- Dairy/2019/201908/20190818-20190824/字符串指针使用/字符串指针使用.cpp
+++ Dairy/2019/201908/20190818-20190824/字符串指针使用/字符串指针使用.cpp
@@ -3,13 +3,6 @@ using namespace std;
 
 int main()
 {
-	char * foo = "this is a foo";
-	char * f = foo;
-	cout << *f; //输出元素
-	cout << f; //字符串变量的指针
-
-
-
-
-	system("pause");
+	int a = max(2, 3);
+	cout << a;
 }
