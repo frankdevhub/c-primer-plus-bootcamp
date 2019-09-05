5609f83da80caa858f2ad026f4f928474227c672
frankdevhub <frankdevhub@gmail.com>
2019-09-06 05:20:24 +0800

    project rebuild

---
+++ Basic/继承与派生：简单的派生类的构造函数/draft.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student
+{
+public:
+	Student(int n,string nam,char s)
+	{
+		num = n;
+		name = nam;
+		sex = s;
+	}
+
+	~Student(){}
+
+protected:
+	int num;
+	string name;
+	char sex;
+};
+
+
+class Student1 :public Student
+{
+public:
+	Student1(int n,string nam,char s,int a,string ad) :Student(n, nam, s)
+	{
+		age = a;
+		addr = ad;
+	}
+
+	void show()
+	{
+		cout << "num:" << num << endl;
+		cout << "name:" << name << endl;
+		cout << "sex:" << sex << endl;
+		cout << "age:" << age << endl;
+		cout << "address:" << addr << endl;
+	}
+private:
+	int age;
+	string addr;
+};
+
+
+
+int main()
+{
+	Student1 stud1(10010,"Wang-li",'f',19,"115 Beijing Road,Shanghai");
+	stud1.show();
+
+	system("pause");
+}
