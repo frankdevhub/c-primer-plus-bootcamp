18cb1e36aa6a1cc5f97b96f519db78ccb59907e3
frankdevhub <frankdevhub@gmail.com>
2019-09-16 23:54:50 +0800

    project rebuild

---
+++ Network Programming/1. Socket套接字编程/1.1 寻址方式和字节顺序/inet_addr.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+struct in_addr
+{
+	union
+	{
+		struct
+		{
+			unsigned char s_b1, s_b2, s_b3, s_b4;
+		} S_un_b;
+		struct
+		{
+			unsigned short s_w1, s_w2;
+		} S_un_w;
+		unsigned long S_addr;
+	} S_un;
+	
+};
+
+struct scokaddr_in
+{
+	short			sin_family;
+	unsigned short  sin_port;
+	struct in_addr  sin_addr;
+	char			sin_zero[8];
+
+};
+
+
+int main()
+{
+	scokaddr_in addr;
+	addr.sin_addr.S_un.S_addr = inet_addr("218.6.132.5");
+}
---
+++ Network Programming/1. Socket套接字编程/1.2 WinSock网络程序开发流程/frankdevhub-winsock/winsock-client.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+using namespace std;
+
